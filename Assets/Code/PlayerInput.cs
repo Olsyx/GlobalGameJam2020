39934c9b4fdbe2f971966e65511e32c3f6b1e3de
Olatz <darkatomish@gmail.com>
2020-01-31 22:45:26 +0100

    feature: Transmitter + Receiver + PlayerInput

---
+++ Assets/Code/PlayerInput.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GGJ.IO {
+    public class PlayerInput : Transmitter {
+
+        [Header("Behaviour")]
+        public Transform aim;
+        public float maxDistance = 2.5f;
+
+        [Header("Buttons")]
+        public string leftHandButton = "LeftClick";
+        public string leftHandInteraction = "LeftHand";
+        public string rightHandButton = "RightClick";
+        public string rightHandInteraction = "RightHand";
+        public string upButton = "Up";
+        public string downButton = "Down";
+
+        Receiver cachedReceiver;
+
+        #region Mono Behaviour
+        private void FixedUpdate() {
+            FindTarget();
+            ManageUnityInput();
+        }
+        #endregion
+
+        #region Control
+        private void FindTarget() {
+            RaycastHit hit;
+            if (!Physics.Raycast(transform.position, GetAimVector(), out hit, maxDistance)) {
+                return;
+            }
+
+            Receiver newReceiver = hit.collider.GetComponentInParent<Receiver>(); ;
+
+            if (cachedReceiver == newReceiver) {
+                SendAction(hoverActions, cachedReceiver);
+                return;
+            }
+
+            Remove(cachedReceiver);
+            SendAction(exitActions, cachedReceiver);
+            cachedReceiver = newReceiver;
+            Register(cachedReceiver);
+            SendAction(enterActions, cachedReceiver);
+        }
+
+        private void ManageUnityInput() {
+            if (Input.GetButtonUp(rightHandButton)) {
+                SendAction(rightHandInteraction);
+            }
+
+            if (Input.GetButtonUp(leftHandButton)) {
+                SendAction(leftHandInteraction);
+            }
+
+            float vertical = Input.GetAxis("Vertical");
+            float horizontal = Input.GetAxis("Horizontal");
+
+            if (vertical > 0) {
+                SendAction("Forward");
+            } else if (vertical <= 0) {
+                SendAction("Backwards");
+            }
+
+            if (horizontal > 0) {
+                SendAction("Right");
+            } else if (horizontal <= 0) {
+                SendAction("Left");
+            }
+
+            if (Input.GetButton(upButton)) {
+                SendAction("Up");
+            } else if (Input.GetButton(downButton)) {
+                SendAction("Down");
+            }
+        }
+        #endregion
+
+        #region Queries
+        private Vector3 GetAimVector() {
+            return  aim.position - transform.position;
+        }
+        #endregion
+
+        #region Debug
+        private void OnDrawGizmos() {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(transform.position, transform.position + GetAimVector() * maxDistance);
+        }
+        #endregion
+
+
+    }
+}
---
+++ Assets/Code/Transmitter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace GGJ.IO {
+    public abstract class Transmitter : MonoBehaviour {
+
+        [SerializeField] protected string id;
+        public List<string> enterActions = new List<string>() { "Enter" };
+        public List<string> hoverActions = new List<string>() { "Hover" };
+        public List<string> exitActions = new List<string>() { "Exit" };
+
+        public string Id { get => id; }
+        protected List<Receiver> receivers = new List<Receiver>();
+             
+        public void Register(Receiver receiver) {
+            if (receivers.Contains(receiver)) {
+                return;
+            }
+            receivers.Add(receiver);
+        }
+
+        public void Remove(Receiver receiver) {
+            receivers.Remove(receiver);
+        }
+
+        public void SendAction(List<string> actions) {
+            for (int i = 0; i < receivers.Count; i++) {
+                SendAction(actions, receivers[i]);
+            }
+        }
+
+        public void SendAction(string action) {
+            for (int i = 0; i < receivers.Count; i++) {
+                SendAction(action, receivers[i]);
+            }
+        }
+
+        public void SendAction(List<string> actions, Receiver target) {
+            if (target == null) {
+                return;
+            }
+            for (int i = 0; i < actions.Count; i++) {
+                SendAction(actions[i], target);
+            }
+        }
+        public void SendAction(string action, Receiver target) {
+            if (target == null) {
+                return;
+            }
+            target.Receive(action);
+        }
+    }
+}
