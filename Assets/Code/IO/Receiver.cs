2e7093a541738ae8b27ba4ea64f025e8bed1d1ad
Olatz <darkatomish@gmail.com>
2020-02-01 00:15:30 +0100

    wip: Mechanic

--- Assets/Code/IO/Receiver.cs
+++ Assets/Code/IO/Receiver.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 
 namespace GGJ.IO {
+    public interface IReceiverListener {
+        void Receive(string action);
+    }
+
     public class Receiver : MonoBehaviour {
 
         [SerializeField] protected string id;
@@ -12,8 +16,27 @@ namespace GGJ.IO {
 
         public string Id { get => id; }
 
+        protected List<IReceiverListener> listeners = new List<IReceiverListener>();
+
+        public void Register(IReceiverListener listener) {
+            if (listeners.Contains(listener)) {
+                return;
+            }
+            listeners.Add(listener);
+        }
+
+        public void Remove(IReceiverListener listener) {
+            listeners.Remove(listener);
+        }
+        
         public virtual void Receive(string action) {
-            Debug.Log(action);
+            if (!enabled) {
+                return;
+            }
+
+            for (int i = 0; i < listeners.Count; i++) {
+                listeners[i].Receive(action);
+            }
         }
 
 
---
+++ Assets/Code/Mechanic.cs
@@ -0,0 +1,61 @@
+﻿using GGJ.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ.Core {
+    public abstract class Mechanic : MonoBehaviour, IReceiverListener {
+
+        public Receiver receiver;
+
+        #region Mono Behaviour
+        private void Awake() {
+            StoreProperties();
+            Init();
+        }
+
+        private void Start() {
+            StoreReferences();
+            SetUp();
+        }
+
+        private void OnDestroy() {
+            receiver.Remove(this);
+        }
+        #endregion
+
+        #region Init
+        protected virtual void StoreProperties() {
+            receiver = receiver ?? GetComponentInParent<Receiver>();
+        }
+
+        protected virtual void Init() {
+
+        }
+
+        protected virtual void StoreReferences() {
+
+        }
+
+        protected virtual void SetUp() {
+            receiver.Register(this);
+        }
+        #endregion
+
+        #region Control
+        public abstract void Receive(string action);
+        #endregion
+
+        #region Actions
+
+        #endregion
+
+        #region Queries
+
+        #endregion
+
+        #region Debug
+
+        #endregion
+    }
+}
