d8103d1a55164efee6ed9bb5a17de383237fbc99
Olatz <darkatomish@gmail.com>
2020-02-02 05:18:59 +0100

    feature: MotorOrderAnimation

---
+++ Assets/Code/MotorOrderAnimation.cs
@@ -0,0 +1,31 @@
+﻿using GGJ.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GGJ.Behaviours {
+
+    public class MotorOrderAnimation : MonoBehaviour {
+        public List<MaterialChanger> slots = new List<MaterialChanger>();
+        public List<AudioSource> sounds = new List<AudioSource>();
+        public List<float> delays = new List<float>() { 1, 1, 1, 1, 1 };
+        public List<int> order = new List<int>() { 0, 4, 1, 3, 2 };
+
+        public void Play() {
+            float delay = 0;
+            for (int i = 0; i < order.Count; i++) {
+                StartCoroutine(Show(order[i], delay));
+                delay += delays[order[i]];
+            }
+        }
+
+        IEnumerator Show(int index, float addedDelay) {
+            yield return new WaitForSeconds(addedDelay);
+            slots[index].SetEmissive(1);
+            sounds[index].Play();
+            yield return new WaitForSeconds(delays[index]);
+            slots[index].SetEmissive(0);
+        }
+    }
+
+}
